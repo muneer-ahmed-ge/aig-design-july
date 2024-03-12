90274d330319b23659fc56850499bcb3330ffa5f
muahmed <muahmed@ptc.com>
2024-03-11 19:20:20 -0700

    بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ

--- sql/search.py
+++ sql/search.py
@@ -14,8 +14,14 @@ embeddings = AzureOpenAIEmbeddings(
 persist_directory = "/Users/muahmed/MT/ai/aig-design-july/resources/metadata_collection"
 db = Chroma(persist_directory=persist_directory, embedding_function=embeddings, collection_name="metadata_collection")
 
-query = "Who was the last technician for WO-00000450 ?"
-docs = db.similarity_search(query=query, k=5)
+question = "Who was the last technician for WO-00000450 ?"
+question = "Who was the last technician for WO-00000450 ?"
+question = "Find out WO-00000450 total Work Order Lines of Line Type = 'Labor' ?"
+question = "How many work order exists ?"  # SELECT COUNT(*) FROM SVMXC__Service_Order__c
+question = "What is the most common Line Type of Work Order Line"  # SELECT SVMXC__Line_Type__c, COUNT(*) as count FROM SVMXC__Service_Order_Line__c GROUP BY SVMXC__Line_Type__c ORDER BY count DESC LIMIT 1
+question = "What are the Line Types for WO-00000450 ?"
+
+docs = db.similarity_search(query=question, k=5)
 for d in docs:
     e = d.page_content
     idx1 = e.find("ObjectName: ")
