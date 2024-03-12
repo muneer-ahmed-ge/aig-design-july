04ac2b2af43ebfa4e93383809185cbee874dc6eb
muahmed <muahmed@ptc.com>
2024-03-11 18:28:04 -0700

    بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ

--- sql/query.py
+++ sql/query.py
@@ -70,8 +70,8 @@ chain = create_sql_query_chain(llm, db, prompt=PROMPT)
 question = "Who was the last technician for WO-00000450 ?"
 question = "Find out WO-00000450 total Work Order Lines of Line Type = 'Labor' ?"
 question = "How many work order exists ?"  # SELECT COUNT(*) FROM SVMXC__Service_Order__c
-question = "What are the Line Types for WO-00000450 ?"
 question = "What is the most common Line Type of Work Order Line"  # SELECT SVMXC__Line_Type__c, COUNT(*) as count FROM SVMXC__Service_Order_Line__c GROUP BY SVMXC__Line_Type__c ORDER BY count DESC LIMIT 1
+question = "What are the Line Types for WO-00000450 ?"
 
 response = chain.invoke({"question": question})
 
