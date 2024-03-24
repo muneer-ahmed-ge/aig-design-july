ee98e3f5f9ff1c3fb3542b7158ed0a74b47938d9
muahmed <muahmed@ptc.com>
2024-03-23 20:42:58 -0700

    بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ

--- orchestration/agent-tool-chain.py
+++ orchestration/agent-tool-chain.py
@@ -54,8 +54,8 @@ agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 context = "WO-00000450"
 question = "How to fix this asset Xerox Printer"
 question = "Can you schedule next preventative work order maintenance of this Asset"
-question = "Can you schedule work order WO-00000450 to the tech that has mostly worked on the Asset Xerox Printer"
 question = "Can you tell me who has mostly worked on the Asset Xerox Printer"
+question = "Can you schedule work order WO-00000450 to the tech that has mostly worked on the Asset Xerox Printer"
 
 answer = agent_executor.invoke(
     {
