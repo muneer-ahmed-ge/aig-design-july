4b48bf34767ffc582d3983c36ecf468a80bf8bdd
muahmed <muahmed@ptc.com>
2024-04-08 18:14:20 -0700

    بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ

--- orchestration/tools.py
+++ orchestration/tools.py
@@ -3,33 +3,26 @@ from langchain_core.tools import tool
 @tool
 def query_record_by_name(record_name: str) -> str:
     """API for fetching the record given record name"""
-    print("**tool query_record_by_name**, Input Record Name = " + record_name)
-    return "{'tech' : 'tom'}"
+    return input('Query Record Question > ' + record_name + ' :  ')
 
 
 @tool
 def service_history(history_question: str) -> str:
     """API for Service History pass the entire user input"""
-    print("**tool service_history** Service History Skills, Input Question = " + history_question)
-    return "Tom"
-
+    return input('Service History Question > ' + history_question + ' :  ')
 
 @tool
 def scheduling(scheduling_question: str) -> str:
     """API for Schedule Management pass the entire user input"""
-    print("**tool scheduling ** Scheduling Skills, Input Question = " + scheduling_question)
-    return "Done"
+    return input('Scheduling Skill Question > ' + scheduling_question + ' :  ')
 
 
 @tool
 def get_product_id(product_name: str) -> str:
     """API for fetching the product id give the product name"""
-    print("**tool get_product_id**, Input Product Name = " + product_name)
-    return "PR-007"
-
+    return input('Get Product Id Question > ' + product_name + ' :  ')
 
 @tool
 def knowledge(product_id: str) -> str:
     """API for Product Documentation provided Product id, first fetch the product id and then come here"""
-    print("**TOOL** knowledge, Input Product Id = " + product_id)
-    return "Remove the jammed papers and restart the machine"
+    return input('Knowledge Skill Question > ' + product_id + ' :  ')
