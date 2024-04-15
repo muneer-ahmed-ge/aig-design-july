9cb3d60feaa7713e646812fe433f4f3772a473b5
muahmed <muahmed@ptc.com>
2024-04-15 16:55:28 -0700

    بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ

--- aig/agent.py
+++ aig/agent.py
@@ -1,4 +1,5 @@
-# Goal : https://servicemax.atlassian.net/wiki/spaces/PROD/pages/3951984679/Copilot+Chat+Examples
+# Goal :https://servicemax.atlassian.net/wiki/spaces/AE/pages/3968860163/Agent+Tools
+# Reference : https://servicemax.atlassian.net/wiki/spaces/PROD/pages/3951984679/Copilot+Chat+Examples
 # Subject Identification : https://servicemax.atlassian.net/browse/AIG-519
 # Service History Tools : https://servicemax.atlassian.net/browse/AIG-617
 
@@ -11,9 +12,9 @@ from langchain_community.chat_models.azure_openai import AzureChatOpenAI
 from langchain_core.messages import HumanMessage, AIMessage
 
 from aig.tools import query_records_by_name, get_work_order_id_by_name, get_work_order_for_installed_product, \
-    get_installed_product_for_work_order, get_product_by_name, \
+    get_installed_product_for_work_order, get_product_id_for_installed_product_id, get_product_by_name, \
     get_service_history_for_work_order_id, get_service_history_for_installed_product_id, \
-    get_schedule_management, get_knowledge_access, get_service_history
+    schedule_management, get_knowledge_access
 
 load_dotenv()
 llm = AzureChatOpenAI(azure_endpoint="https://smax-ai-dev-eastus.openai.azure.com",
@@ -29,12 +30,6 @@ PROMPT_PREFIX = """
     A technician named {user_name} is chatting with you now.
 
     The Asset Service history records consist of Work Orders, Work Details, Installed Products, and assigned technicians
-
-    The Work Order and Installed Product are related. Asset is another name for Installed Product.
-    The Work Order name pattern is WO-00000492 and its id starts with a2D and it is 18 digit alphanumeric example a2De0000005hBeyEAE 
-    The Installed Product id starts with a0N and it is 18 digit alphanumeric example a0Ne000001nDzGiEAK  
-    
-    If the Work Order name is WO-00000155 then its id a2De0000005gpjGEAQ and its Installed Product is a0Ne000001nDzCdEAK
     
     Pass the complete question to each tool.
     
@@ -48,55 +43,15 @@ prompt = hub.pull("hwchase17/openai-tools-agent")
 system_prompt = prompt.messages[0]
 system_prompt.prompt.template = prefix + "\n\n" + system_prompt.prompt.template
 
-tools_context = [get_service_history, get_schedule_management, get_knowledge_access]
-
-tools_no_context = [query_records_by_name, get_work_order_id_by_name, get_work_order_for_installed_product,
-                    get_installed_product_for_work_order, get_product_by_name,
-                    get_service_history_for_work_order_id, get_service_history_for_installed_product_id,
-                    get_schedule_management]
-
-tools = tools_no_context
+tools = [
+    query_records_by_name, get_work_order_id_by_name, get_work_order_for_installed_product,
+    get_installed_product_for_work_order, get_product_id_for_installed_product_id, get_product_by_name,
+    get_service_history_for_work_order_id, get_service_history_for_installed_product_id,
+    schedule_management, get_knowledge_access
+]
 
 agent = create_openai_tools_agent(tools=tools, llm=llm, prompt=prompt)
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=False)
-
-"""
-Non Context Based Questions 
----------------------------
-when was this Asset installed ?
-
-when was Asset Ultrasound Logic installed ?
-{"records":[{"type":"SVMXC__Installed_Product__c","id":"poHo0000027ToWIAU","name":"Ultrasound Logic 2 series"},{"type":"SVMXC__Installed_Product__c","id":"poHo0000027ToWXYZ","name":"Ultrasound Logic 52 series"}]}
-
-when was Asset of WO-00000155 installed ?
-ipg1U000000xYk8QAE
-
-Question : What’s on my calendar today ?
-Appointment: [WO-00000155] Princess Margaret Hospital on April 10, 2024 2pm and Appointment: [WO-00008627] United Oil & Gas Corp on April 10, 2024 at 4pm
-Question : Who was was the last tech for first appointment and what was the problem with second appointment and how to fix first appointment's red light flashing ?
-John Doe
-Oil Leakage
-Restart the machine 
-
-Conversation # 3
-Question : Can you schedule work order WO-00000450 to the tech that has mostly worked on the Asset Xerox Printer ?
-
-Context Based Questions 
------------------------
-When was this machine installed ?
-April 20, 2023
-
-What's my schedule this afternoon ?
-Appointment: [WO-00000155] Princess Margaret Hospital on April 10, 2024 2pm and Appointment: [WO-00008627] United Oil & Gas Corp on April 10, 2024 at 4pm
-
-What is the frequency range for the sensor on this machine ?
-120 meters
-"""
-# woQ4O000003R29MUAS
-# ipg1U000000xYk8QAE
-# poHo0000027ToWIAU
-# You have a 9:00 AM job at Good Samaritan Hospital WO-12345, a 12:30 PM job at Valley Medical Center, and WO-000789 3:00 PM job at Northridge Health Associates.
-# Sure thing. I found several potential times for the job. Please select one of the options (1) February 22nd, 2024 at 9:00 AM (2) February 24th, 2024 at 2:00 PM
 
 question = ""
 chat_history = []
--- aig/tools.py
+++ aig/tools.py
@@ -1,40 +1,29 @@
 from langchain_core.tools import tool
 
 
-# Service Tools
-@tool
-def get_service_history(question: str) -> str:
-    """API for getting Service History"""
-    return input('get_service_history, question = ' + question + ' > enter answer : ')
-
-
-@tool
-def get_schedule_management(question: str) -> str:
-    """API for Schedule Management"""
-    return input(
-        'get_schedule_management question = ' + question + ' > enter answer : ')
-
-
-@tool
-def get_knowledge_access(question: str) -> str:
-    """API for Knowledge and help documentation"""
-    return input('get_knowledge_access > question = ' + question + ' > enter Answer >> ')
-
+# Services Tools
 
 @tool
 def get_service_history_for_work_order_id(work_order_id: str, question: str) -> str:
     """API for getting Service History about work order id which must start with must start with a2D and of length 15 digit a2De0000005gpjGEAQ"""
-    payload = '''{"entity_resource": "SVMXC__Service_Order__c","entity": "%s"}''' % (work_order_id)
-    return input('get_service_history_for_work_order, ' + payload + ' question = '
-                 + question + ' > enter answer : ')
+    payload = '''{"tool_name":"get_service_history_for_work_order_id","parameters":{"work_order_id":"%s", question":"%s"}}''' % (
+        work_order_id, question)
+    return input(payload + ' > enter answer : ')
 
 
 @tool
 def get_service_history_for_installed_product_id(installed_product_id: str, question: str) -> str:
     """API for getting Service History about installed product id"""
-    payload = '''{"entity_resource": "SVMXC__Installed_Product__c","entity": "%s"}''' % (installed_product_id)
+    payload = '''{"tool_name":"get_service_history_for_installed_product_id","parameters":{"installed_product_id":"%s", question":"%s"}}''' % (
+        installed_product_id, question)
+    return input(payload + ' > enter answer : ')
+
+
+@tool
+def schedule_management(question: str) -> str:
+    """API for Schedule Management"""
     return input(
-        'get_service_history_for_installed_product_id, ' + payload + ' question = ' + question + ' > enter answer : ')
+        'get_schedule_management question = ' + question + ' > enter answer : ')
 
 
 @tool
@@ -48,32 +37,46 @@ def get_knowledge_access(product: str, question: str) -> str:
 # System Tools
 
 @tool
-def query_records_by_name(name: str) -> str:
-    """API for searching records given name"""
-    return input('query_records_by_name, name = ' + name + ' > enter answer : ')
-
-
-@tool
 def get_work_order_id_by_name(work_order_name: str) -> str:
     """API for getting the work order id given work order name like WO-00000000"""
-    return input('get_work_order_id_by_name, work_order_name = ' + work_order_name + ' > enter answer : ')
+    payload = '''{"tool_name":"get_work_order_id_by_name","parameters":{"work_order_name":"%s"}}''' % (work_order_name)
+    return input(payload + ' > enter answer : ')
 
 
 @tool
 def get_work_order_for_installed_product(installed_product_name: str) -> str:
     """API for getting work order for installed product name eg: Ultrasound Logic series 2"""
-    return input(
-        'get_work_order_for_installed_product, installed_product_name = ' + installed_product_name + ' > enter answer : ')
+    payload = '''{"tool_name":"get_work_order_for_installed_product","parameters":{"installed_product_name":"%s"}}''' % (
+        installed_product_name)
+    return input(payload + ' > enter answer : ')
+
+
+@tool
+def query_records_by_name(name: str) -> str:
+    """API for searching records given name"""
+    payload = '''{"tool_name":"query_records_by_name","parameters":{"name":"%s"}}''' % (name)
+    return input(payload + ' > enter answer : ')
 
 
 @tool
 def get_installed_product_for_work_order(work_order_name: str) -> str:
     """API for getting installed product given work order name like WO-00000000"""
-    return input(
-        'get_installed_product_for_work_order, work_order_name = ' + work_order_name + ' > enter answer : ')
+    payload = '''{"tool_name":"get_installed_product_for_work_order","parameters":{"work_order_name":"%s"}}''' % (
+        work_order_name)
+    return input(payload + ' > enter answer : ')
+
+
+@tool
+def get_product_id_for_installed_product_id(installed_product_id: str) -> str:
+    """API for getting product id given installed product id"""
+    payload = '''{"tool_name":"get_product_id_for_installed_product_id","parameters":{"installed_product_id":"%s"}}''' % (
+        installed_product_id)
+    return input(payload + ' > enter answer : ')
 
 
 @tool
 def get_product_by_name(product_name: str) -> str:
     """API for getting product given product name"""
-    return input('get_product_by_name, product_name = ' + product_name + ' > enter Answer >> ')
+    payload = '''{"tool_name":"get_product_by_name","parameters":{"product_name":"%s"}}''' % (
+        product_name)
+    return input(payload + ' > enter answer : ')
