884060ded8140a43015c78fe00434aefdf1ccf75
muahmed <muahmed@ptc.com>
2024-04-17 22:25:11 -0700

    بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ

--- aig/tools.py
+++ aig/tools.py
@@ -69,10 +69,6 @@ def schedule_management(question: str) -> str:
 
 
 def print_tool(tool_name: str, parameter_name: str, parameter_value: str, question=None) -> str:
-    payload = '''tool_name=%s, parameters %s=%s > Rephrased Question = %s\n''' % (tool_name, parameter_name, parameter_value, question)
-    # payload = "tool_name=%s " % tool_name
-    # if parameter_name is not None:
-    #     payload += " parameters %s=%s" % (parameter_name, parameter_value)
-    # if question is not None:
-    #     payload += " Rephrased Question=%s" % (question)
+    payload = '''tool_name=%s, parameters %s=%s > Rephrased Question = %s\n''' % (
+    tool_name, parameter_name, parameter_value, question)
     return input(payload + 'Answer (enter) : ')
